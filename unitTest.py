0d2eef57067216eb1365f2638d0aa29e36ce97c1
iannico322 <73187566+iannico322@users.noreply.github.com>
2023-05-20 18:06:28 +0800

    hehe

--- unitTest.py
+++ unitTest.py
@@ -3,8 +3,16 @@
 
 import unittest
 
+class CustomError(Exception):
+    pass
+
+
 def mult(x, y):
-    return float(x) * float(y)
+    if isinstance(x, str) or isinstance(y, str):
+        return "gay you input string"
+    return x * y
+ 
+
 
 
 class TestMult(unittest.TestCase):
@@ -20,10 +28,11 @@ class TestMult(unittest.TestCase):
         result = mult(5, 0)
         self.assertEqual(result, 0)
         
-    def test_mult_2string_numbers(self):
-        result = mult("2", "5")
-        self.assertEqual(result, 10)
-    
+    def test_mult_2strings(self):
+        result = mult("5", "0")
+        self.assertEqual(result,"gay you input string")
+  
+ 
            
 if __name__ == '__main__':
     unittest.main()
